3f2da17cde970d2d48582c5a48e0079655b7025c
nicoletanyt <nicole_tan_yitong@s2021.ssts.edu.sg>
2023-12-07 17:48:42 +0800

    finished day 7 part 1 and 2

---
+++ DAY 7/part1.py
@@ -0,0 +1,62 @@
+from functools import cmp_to_key
+
+with open('input.txt') as f:
+    lines = [line.rstrip() for line in f]
+
+cards = ["A", "K", "Q", "J", "T", "9", "8", "7", "6", "5", "4", "3", "2"] # larger index, weaker
+rank = {
+    1: [],
+    2: [],
+    3: [],
+    4: [],
+    5: [],
+    6: [],
+    7: []
+}
+
+for i in lines:
+    hand = i.split(" ")
+    temp = {}
+    for j in range(0, len(hand[0])):
+        if hand[0][j] in temp:
+            temp[hand[0][j]] += 1
+        else:
+            temp[hand[0][j]] = 1
+
+    vals = list(temp.values())
+    vals.sort()
+    if vals[0] == 5:
+        rank[7].append((hand[0], hand[1]))
+    elif vals == [2, 3]:
+        rank[5].append((hand[0], hand[1]))
+    elif vals == [1, 1, 1, 1, 1]:
+        rank[1].append((hand[0], hand[1]))
+    elif vals == [1, 1, 1, 2]:
+        rank[2].append((hand[0], hand[1]))
+    elif vals == [1, 1, 3]:
+        rank[4].append((hand[0], hand[1]))
+    elif vals == [1, 2, 2]:
+        rank[3].append((hand[0], hand[1]))
+    elif vals == [1, 4]:
+        rank[6].append((hand[0], hand[1]))
+
+total = 0
+curr = 0
+
+def compareHands(a, b):
+    for j in range(0, 5):
+        diff = cards.index(a[0][j]) - cards.index(b[0][j])
+        if diff != 0:
+            return -diff
+
+for i in rank.keys():
+    if len(rank[i]) == 1:
+        curr += 1
+        total += curr * int(rank[i][0][1])
+    elif len(rank[i]) > 1:
+        sorted_hands = sorted(rank[i], key=cmp_to_key(compareHands))
+        for j in sorted_hands:
+            curr += 1
+            total += curr * int(j[1])
+
+print(total)
