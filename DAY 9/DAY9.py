5f1985dac6769a28b920146d45904778ea497e5e
nicoletanyt <nicole_tan_yitong@s2021.ssts.edu.sg>
2023-12-14 18:34:57 +0800

    finished day 9

---
+++ DAY 9/DAY9.py
@@ -0,0 +1,19 @@
+with open('input.txt') as f:
+    lines = [line.rstrip() for line in f]
+
+x = 0
+
+for i in range(0, len(lines)):
+    nums = lines[i].split(" ")
+    # nums.reverse() <- ADD THIS LINE FOR PART 2 
+    temp = nums
+    x += int(nums[-1])
+    while temp != [0] * len(temp):
+        seq = []
+        for j in range(0, len(temp) - 1):
+            seq.append(int(temp[j + 1]) - int(temp[j]))
+
+        temp = seq
+        x += int(temp[-1])
+
+print(x)
