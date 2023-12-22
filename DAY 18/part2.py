57a2f410eaadbf4596896a9841fcf7f4babc741d
nicoletanyt <nicole_tan_yitong@s2021.ssts.edu.sg>
2023-12-22 20:25:55 +0800

    finished day 18 part 2

--- DAY 18/part2.py
+++ DAY 18/part2.py
@@ -6,19 +6,24 @@ trench_pos = []
 
 for i in range(0, len(lines)):
     parts = lines[i].split(" ")
+    rgb = [int(x, 16) for x in parts[2][2:7]]
 
-    if parts[0] == "R":
-        for j in range(1, int(parts[1]) + 1):
+    dist = rgb[0] * 16 ** 4 + rgb[1] * 16 ** 3 + rgb[2] * 16 ** 2 + rgb[3] * 16 ** 1 + rgb[4]
+    direc = parts[2][7]
+
+    if direc == "0": # R
+        for j in range(1, dist + 1):
             trench_pos.append((pos[0], pos[1] + j))
-    elif parts[0] == "L":
-        for j in range(1, int(parts[1]) + 1):
+    elif direc == "2": # L
+        for j in range(1, dist + 1):
             trench_pos.append((pos[0], pos[1] - j))
-    elif parts[0] == "U":
-        for j in range(1, int(parts[1]) + 1):
+    elif direc == "3": # U
+        for j in range(1, dist + 1):
             trench_pos.append((pos[0] - j, pos[1]))
-    elif parts[0] == "D":
-        for j in range(1, int(parts[1]) + 1):
+    elif direc == "1": # D
+        for j in range(1, dist + 1):
             trench_pos.append((pos[0] + j, pos[1]))
+    
     pos = trench_pos[-1]
 
 prev_row = trench_pos[0][0]
