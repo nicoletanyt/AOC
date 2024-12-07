662257ffcaaf562c6152e85bc55c077ab3a350c4
nicole <nicole_tan_yitong@s2021.ssts.edu.sg>
2024-12-07 21:12:36 +0800

    finished day 7 part 1 and 2

---
+++ 2024/DAY 7/part1.py
@@ -0,0 +1,24 @@
+with open('input.txt') as f:
+    lines = [line.rstrip() for line in f]
+
+total = 0
+
+for l in lines:
+    # parse the data
+    result, n = l.split(": ")
+    numbers = [int(i) for i in n.split(" ")]
+    result = int(result)
+    n = len(numbers)
+
+    def recur(current: int, index: int):
+        # return True if this is the last element 
+        if current == result and index == n: return True
+        if current > result or index >= n: return False
+        
+        # otherwise, multiply or add it again 
+        return recur(current * numbers[index], index + 1) or recur(current + numbers[index], index + 1)
+
+    if recur(1, 0):
+        total += result
+
+print(total)
