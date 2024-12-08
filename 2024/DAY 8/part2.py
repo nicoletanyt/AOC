27b0242f0ff86dc87cd78ca8692d02cfec75fe37
nicole <nicole_tan_yitong@s2021.ssts.edu.sg>
2024-12-08 22:36:36 +0800

    finished day 8 part 1 and 2

---
+++ 2024/DAY 8/part2.py
@@ -0,0 +1,44 @@
+with open('input.txt') as f:
+    lines = [line.rstrip() for line in f]
+
+antennas = {} # stores the coords in a dict
+antinodes = set()
+MAX_ROW = len(lines)
+MAX_COL = len(lines[0])
+
+# parse the data
+for i in range(0, len(lines)):
+    for j in range(0, len(lines[i])):
+        if lines[i][j].isalnum():
+            # this is an antenna
+            if lines[i][j] in antennas:
+                antennas[lines[i][j]].append((i, j))
+            else:
+                antennas[lines[i][j]] = [(i, j)]
+
+for freq in antennas:
+    coords = antennas[freq]
+    for i in range(0, len(coords)):
+        for j in range(i + 1, len(coords)):
+            # find new antinodes
+            displacement = (coords[i][0] - coords[j][0], coords[i][1] - coords[j][1])
+
+            curr = 0
+            new_anticode = (coords[i][0] + displacement[0] * curr, coords[i][1] + displacement[1] * curr)
+
+            # ensure that the coords is within range
+            while new_anticode[0] >= 0 and new_anticode[0] < MAX_ROW and new_anticode[1] >= 0 and new_anticode[1] < MAX_COL:
+                antinodes.add(new_anticode)
+                curr += 1
+                new_anticode = (coords[i][0] + displacement[0] * curr, coords[i][1] + displacement[1] * curr)
+            
+            curr = 0
+            new_anticode = (coords[j][0] - displacement[0] * curr, coords[j][1] - displacement[1] * curr)
+
+            while new_anticode[0] >= 0 and new_anticode[0] < MAX_ROW and new_anticode[1] >= 0 and new_anticode[1] < MAX_COL:
+                antinodes.add(new_anticode)
+                curr += 1
+                new_anticode = (coords[j][0] - displacement[0] * curr, coords[j][1] - displacement[1] * curr)
+
+
+print(len(antinodes))
